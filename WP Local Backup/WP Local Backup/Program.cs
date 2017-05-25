32a9dbef12e9f9108d8f7bdea334f1b514733854
Christopher Piontek <cp@bitpage.de>
2017-05-25 18:14:26 +0200

    initial commit with my program idea

---
+++ WP Local Backup/WP Local Backup/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WP_Local_Backup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to " + Assembly.GetExecutingAssembly().GetName().Name + " " + Assembly.GetExecutingAssembly().GetName().Version);
+
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1. Make a backup");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
